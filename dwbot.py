0e00bfe643bdf041ddd2cad0a83cef2d48162ff1
Sanky <gsanky@gmail.com>
2011-04-18 15:30:23 +0200

    Only need to log in once, using getpass for password input

--- dwbot.py
+++ dwbot.py
@@ -6,17 +6,14 @@ import http.client
 from urllib.parse import urlencode
 import json
 
-if len(argv)<3:
-    sys.exit("Usage: python3 dwbot.py username password")
 
 class DWSession():
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.TOKEN = None
+    def __init__(self):
+        self.token = None
+        self.PMDSUSSID = None
         self.conn = http.client.HTTPConnection("en.pokemon-gl.com")
         
-    def get_PMDSUSSID(self):
+    def get_PMDSUSSID(self, username, password):
         form_conn = http.client.HTTPSConnection("sso.pokemon.com")
         form_conn.request("GET", "/sso/login?service=https://www.pokemon.com/us/account/pgllogin&locale=en")
         r = form_conn.getresponse()
@@ -24,13 +21,13 @@ class DWSession():
         def get_string(s):
             ret = data[data.find(s)+len(s):]
             return ret[:ret.find('"')]
-        params = {"lt":get_string('<input type="hidden" name="lt" value="'), 'username':self.username, 'password':self.password, '_eventId':'submit', 'service':''}
+        params = {"lt":get_string('<input type="hidden" name="lt" value="'), 'username':username, 'password':password, '_eventId':'submit', 'service':''}
         form_conn.request("POST", get_string('<form id="login-form" action="'), urlencode(params), {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"})
         r = form_conn.getresponse()
         data = r.read().decode("utf-8")
         new_url = get_string('top.location.href="')
         if "https://www.pokemon.com/us/account/pgllogin" not in new_url:
-            raise ValueError("Failed to login.")
+            raise ValueError("Wrong username or password.")
         login_conn = http.client.HTTPSConnection("www.pokemon.com")
         def get_relative(url):
             return "/"+"/".join(url.split("/")[3:])
@@ -43,38 +40,31 @@ class DWSession():
         
         return r.getheader("Set-Cookie").split(";")[0][10:]
         
-    def login(self):
-        print("Logging in.")
-        with open("PMDSUSSID", "r") as f:
-            self.PMDSUSSID = f.read().strip()
-        if self.PMDSUSSID == "": 
-            print("Requesting new PMDSUSSID.")
-            self.PMDSUSSID = self.get_PMDSUSSID()
-            with open("PMDSUSSID", "w") as f:
-                f.write(self.PMDSUSSID)
+    def get_token(self):
+        if self.PMDSUSSID == None:
+            if not exists("PMDSUSSID"):
+                sys.exit("Not logged in.  Run python3 dwbot.py username.")
+            with open("PMDSUSSID", "r") as f:
+                self.PMDSUSSID = f.read().strip()
         
-        init = self.request_page("pgl.top.init", ping="0")
+        init = self.request_page("pgl.top.init")
         self.member = init['member']
-        self.TOKEN = init['token']
-        if self.member == None or self.TOKEN == None:
-            print ("Login failed.  Requesting new PMDSUSSID.")
-            self.PMDSUSSID = self.get_PMDSUSSID()
-            with open("PMDSUSSID", "w") as f:
-                f.write(self.PMDSUSSID)
-            self.login()
+        self.token = init['token']
+        if self.member == None or self.token == None:
+            sys.exit ("Login failed.  Run python3 dwbot.py username.")
         else:
-            print ("Login successful!")
+            print ("Got token!")
+            if self.member['member_savedata_id'] == None:
+                sys.exit("Please enter the dream world normally first.")
             print("{} - game {} ({}), Pokémon {}".format(self.member['pgl_name'], self.member['rom_name'], self.member['player_name'], self.member['pokemon_name']))
 
-    def request_page(self, p, member_id= None, ping= None):
+    def request_page(self, p, member_id= None):
         url = "/api/?"
         get = [("p", p)]
-        if ping != None:
-            get.append(("ping", ping))
         if member_id != None:
             get.append(("member_savedata_id", member_id))
-        if self.TOKEN != None:
-            get.append(("token", self.TOKEN))
+        if self.token != None:
+            get.append(("token", self.token))
         url += urlencode(get)
         self.conn.request("GET", url, None, {"Cookie": "PMDSUSSID={}; locale=en".format(self.PMDSUSSID)})
         r = self.conn.getresponse()
@@ -85,6 +75,18 @@ class DWSession():
             raise RuntimeError(error['code'], error['mess'], error['details'])
         return tree
     
+if len(argv)>1:
+    if len(argv)==2:
+        from getpass import getpass
+        password = getpass("Please type your password: ")
+    else:
+        password = argv[2]
+    print("Logging in.")
+    dw = DWSession()
+    PMDSUSSID = dw.get_PMDSUSSID(argv[1], password)
+    with open("PMDSUSSID", "w") as f:
+        f.write(PMDSUSSID)
+    sys.exit("Successfully logged in, run the script without arguments now.")
     
 def print_berry_stats(croft_list):
     harvestable_berries = 0
@@ -95,8 +97,8 @@ def print_berry_stats(croft_list):
     print (" Harvestable berries: {}".format(harvestable_berries))
     print (" Unwatered berries: {}".format(unwatered_berries))
 
-dw = DWSession(argv[1], argv[2])
-dw.login()
+dw = DWSession()
+dw.get_token()
 
 berry_info = dw.request_page("pdw.croft.my_croft_list")
 print_berry_stats(berry_info["croft_list"])
@@ -104,4 +106,3 @@ map_info = dw.request_page("pdw.home.my_island_area")
 print ("List of friends:")
 for friend in map_info ['friend_list']:
     print("{} ({})".format(friend['pgl_name'], friend['country_name']))
-    #print_berry_stats(request_page("pdw.croft.friend_croft_list", friend["member_savedata_id"]))
