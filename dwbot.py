ca0970446365dc65c724e455cb0f5ab40d0f46c1
Sanky <gsanky@gmail.com>
2011-04-18 20:15:15 +0200

    Extended functionality: berries, friends and friend requests; cleanup

--- dwbot.py
+++ dwbot.py
@@ -11,7 +11,6 @@ class DWSession():
     def __init__(self):
         self.token = None
         self.PMDSUSSID = None
-        self.conn = http.client.HTTPConnection("en.pokemon-gl.com")
         
     def get_PMDSUSSID(self, username, password):
         form_conn = http.client.HTTPSConnection("sso.pokemon.com")
@@ -47,27 +46,29 @@ class DWSession():
             with open("PMDSUSSID", "r") as f:
                 self.PMDSUSSID = f.read().strip()
         
-        init = self.request_page("pgl.top.init")
+        init = self.request_page("pgl.top.init", "en")
         self.member = init['member']
         self.token = init['token']
         if self.member == None or self.token == None:
-            sys.exit ("Login failed.  Run python3 dwbot.py username.")
+            sys.exit ("Login failed.  Run python3 dwbot.py username, or copy the PMDSUSSID for pokemon-gl.com from your browser manually, and paste it in PMDSUSSID.")
         else:
             print ("Got token!")
             if self.member['member_savedata_id'] == None:
                 sys.exit("Please enter the dream world normally first.")
             print("{} - game {} ({}), Pokémon {}".format(self.member['pgl_name'], self.member['rom_name'], self.member['player_name'], self.member['pokemon_name']))
 
-    def request_page(self, p, member_id= None):
+    def request_page(self, p, serv="pdw3", **kvargs):
+        conn = http.client.HTTPConnection(serv+".pokemon-gl.com")
         url = "/api/?"
         get = [("p", p)]
-        if member_id != None:
-            get.append(("member_savedata_id", member_id))
+        #if member_id != None:
+        #    get.append(("member_savedata_id", member_id))
         if self.token != None:
             get.append(("token", self.token))
+        get += kvargs.items()
         url += urlencode(get)
-        self.conn.request("GET", url, None, {"Cookie": "PMDSUSSID={}; locale=en".format(self.PMDSUSSID)})
-        r = self.conn.getresponse()
+        conn.request("GET", url, None, {"Cookie": "PMDSUSSID={}; locale=en".format(self.PMDSUSSID)})
+        r = conn.getresponse()
         data = r.read().decode("utf-8")
         tree = json.loads(data)
         if 'error' in tree:
@@ -88,21 +89,28 @@ if len(argv)>1:
         f.write(PMDSUSSID)
     sys.exit("Successfully logged in, run the script without arguments now.")
     
-def print_berry_stats(croft_list):
+def berry_stats(croft_list):
     harvestable_berries = 0
     unwatered_berries = 0
     for berry in croft_list:
-        if berry['kinomi_state']==3: harvestable_berries+=1
+        if berry['kinomi_state']==4: harvestable_berries+=1
         if int(berry['dirt_hp']) < 50: unwatered_berries+=1
-    print (" Harvestable berries: {}".format(harvestable_berries))
-    print (" Unwatered berries: {}".format(unwatered_berries))
-
+    return ("Berries: {} harvestable, {} unwatered, {} total".format(harvestable_berries, unwatered_berries, len(croft_list)))
+    
 dw = DWSession()
 dw.get_token()
-
+visitor_data = dw.request_page("pdw.home.footprint_list", rowcount=28, offset=0)
+requests = []
+for visitor in visitor_data['list']:
+    if visitor['friend_type'] == '1':
+        requests.append(visitor['pgl_name']) 
+print("Friend request from: {}.".format(", ".join(requests)))
 berry_info = dw.request_page("pdw.croft.my_croft_list")
-print_berry_stats(berry_info["croft_list"])
+print(berry_stats(berry_info["croft_list"]))
 map_info = dw.request_page("pdw.home.my_island_area")
 print ("List of friends:")
 for friend in map_info ['friend_list']:
-    print("{} ({})".format(friend['pgl_name'], friend['country_name']))
+    member_id = friend['member_savedata_id']
+    area = dw.request_page("pdw.home.friend_island_area", friend_member_savedata_id=member_id)
+    berries = dw.request_page("pdw.croft.friend_croft_list", member_savedata_id=member_id)['croft_list']
+    print(" {} ({}) - {} friends, {}".format(friend['pgl_name'], friend['country_name'], len(area['friend_list']), berry_stats(berries)))
